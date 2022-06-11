92a29b6396129f6e9b68720792f0aed133ff3926
user <alinazim>
2022-06-11 23:42:20 +0400

    11 Jun 2022

--- BankAtmProject/src/Input/AtmUtil.java
+++ BankAtmProject/src/Input/AtmUtil.java
@@ -4,7 +4,7 @@ public class AtmUtil {
     public static void passwordToEnter() {
         int password = 1994;
         while (true) {
-            int Inputpassword = InputUtil.RequireNumber("Enter the Password: ");
+            int Inputpassword = (int)InputUtil.RequireNumber("Enter the Password: ");
             if (Inputpassword != password) {
                 System.out.println("Wrong password. Try again!");
             }
@@ -15,35 +15,39 @@ public class AtmUtil {
         }
     }
 
-    public static int bal = 6000;
+    private static int balance = 6000;
+
+    public static int getBalance() {
+        return balance;
+    }
 
     public static void WithdrawMoney() {
-        System.out.println("Available Balance: " + bal + "$");
+        System.out.println("Available Balance: " + balance + "$");
         while (true) {
-            int withdraw = InputUtil.RequireNumber("Enter Amount you want to cash out: ");
+            int withdraw = (int) InputUtil.RequireNumber("Enter Amount you want to cash out: ");
 
-            if (withdraw > bal) {
+
+            if (withdraw > balance) {
                 System.out.println("Sufficient balance! ");
-            }
-            if (withdraw <= bal) {
-                int currentBalance = bal- withdraw;
-
+            } else if (withdraw <= balance) {
+                balance = balance - withdraw;
                 System.out.println("You have received " + withdraw + " Thank you for visiting us!");
-                System.out.println("Your current balance: " + currentBalance + "$");
+                System.out.println("Your current balance: " + balance + "$");
                 break;
-            }
+            } else
+                System.out.println("Please enter correct number");
+
 
         }
     }
 
-    public static void Deposit(){
-        int depositMoney= InputUtil.RequireNumber("How much do you want to deposit: ");
-
-
-        System.out.println(c);
+    public static void Deposit() {
+        int depositMoney = (int)InputUtil.RequireNumber("How much do you want to deposit: ");
+        balance = depositMoney + balance;
+        System.out.println("You have received " + depositMoney + " Thank you for visiting us!");
+        System.out.println("Your current balance: " + balance + "$");
     }
 
-    }
 }
 
 
--- BankAtmProject/src/Input/Menyu.java
+++ BankAtmProject/src/Input/Menyu.java
@@ -1,25 +1,28 @@
 package Input;
 
+import Input.*;
+
 public class Menyu {
 
-        public static void SelectMenu() {
-            int menu = 0;
-               menu= InputUtil.RequireNumber("Choose the action: " +
-                        "\n 1. Show the current balance." +
-                        "\n 2. Withdraw money from balance." +
-                        "\n 3. Deposit money." +
-                        "\n 4. Cancel session, return the card");
-                    if (menu == 1) {
-                        System.out.println("Your current balance is "+AtmUtil.bal+"$");
-                    }
-                    if(menu==2){
-                        Input.AtmUtil.WithdrawMoney();
-                    }
-                    if(menu==3){
-
-                    }
-                }
-            }
+    public static void SelectMenu() {
+       int menu = (int)InputUtil.RequireNumber("Choose the action: " +
+                "\n 1. Show the current balance." +
+                "\n 2. Withdraw money from balance." +
+                "\n 3. Deposit money." +
+                "\n 4. Cancel session, return the card");
+        if (menu == 1) {
+            System.out.println("Your current balance is " + AtmUtil.getBalance() + "$");
+        } else if (menu == 2) {
+            AtmUtil.WithdrawMoney();
+        } else if (menu == 3) {
+            AtmUtil.Deposit();
+        } else if (menu == 4) {
+            System.exit(1);
+        } else {
+            System.out.println("Please enter correct number");
+        }
+    }
+}
 
 
 
